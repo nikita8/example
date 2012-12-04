dfc617713e45bf02eb3e55b0103032a8fb36329a
xecutioner <kapilnakhwa@gmail.com>
2012-12-04 15:29:41 +0545

    final payload for example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anatomy::Application.config.secret_token = 'eb384cf4a74eab3b22d04d34013d87ff01b8f2eceb20cbf16add2aee540c5a37c0056c63686f171271a3ec3db4d0ab0017d3b889ff2b92220e4c63185f88a0bd'
